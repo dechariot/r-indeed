ff54bc169479e4a921ed5bf3804f81a08c95431e
dechariot <nsprisang@gmail.com>
2020-04-01 02:27:02 +0700

    Update: You wil can't see the candidates database when authourize is false

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import React from 'react';
+import React, { useState } from "react";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch, Redirect } from "react-router-dom";
+
 //IMPORT PAGES TO APP.JS
 import HomePage from "./pages/HomePage";
 import CandidatesPage from "./pages/CandidatesPage";
 import CandidatePage from "./pages/CandidatePage";
 import CompanyPage from './pages/CompanyPage';
 
-function App() {
+
+const ProtectedRoute = ({ component: Component, user, ...rest }) => {
   return (
-      <Router>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/candidates" exact component={CandidatesPage} />
-        <Route path="/company" exact component={CompanyPage} />
-        <Route path="/candidates/:id/" component={CandidatePage} />
-      </Router>
+    <Route
+      {...rest}
+      render={props => {
+        if (user.isAuthenticated) return <Component {...props} />;
+        return (
+          <Redirect
+            to={{
+              pathname: "/",
+              state: {
+                from: props.location
+              }
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+
+function App() {
+  const [user, setUser] = useState({ isAuthenticated: false });
+  const FourOhFourPage = () => {
+    return (
+      <div>
+        <h1>404 Not Found</h1>
+      </div>
     );
+  };
+  
+
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/company" exact component={CompanyPage} />
+        <ProtectedRoute
+          exact
+          user={user}
+          path="/candidates"
+          component={CandidatesPage} />
+        <ProtectedRoute
+          user={user}
+          path="/candidates/:id"
+          component={CandidatePage} />
+          <Route path="*" component={FourOhFourPage} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
